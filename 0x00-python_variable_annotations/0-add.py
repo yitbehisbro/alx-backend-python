4bc629043e2e38033307121004e7811564ff0782
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-04-27 23:19:21 +0300

    Add floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A type-annotated function add that
+takes a float a and a float b """
+
+
+def add(a: float, b: float) -> float:
+    """returns their sum as a float."""
+    return a + b
