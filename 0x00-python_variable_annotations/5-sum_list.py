c42db464a2932a5dc3db02f96cc1f00be6f9987c
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-04-27 23:53:12 +0300

    Lists sum

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Sum of list """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ Returns sum of list """
+    return sum(input_list)
