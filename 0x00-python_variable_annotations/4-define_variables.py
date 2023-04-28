6db8501587fe756cd1d997068a4cb1f5d5d4b28a
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-04-28 13:10:04 +0300

    Fixed

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -2,8 +2,7 @@
 """ Define variables """
 
 
-if __name__ == "__main__":
-    a: int = 1
-    pi: float = 3.14
-    i_understand_annotations: boolean = True
-    school: str = "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: boolean = True
+school: str = "Holberton"
