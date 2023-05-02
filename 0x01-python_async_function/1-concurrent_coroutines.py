36a1d71c089febda24072144b2c3328328202bf6
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-05-02 09:23:38 +0000

    Error fixed toward the checker

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,10 +5,11 @@ at the same time with async
 import asyncio
 import heapq
 from itertools import chain
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Spawn wait_random n times with the specified max_delay
     and returns the list of all the delays (float values)
     """
