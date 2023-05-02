34b33a60aaf9ecd1a5ac47bd29447cbcd5405795
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-05-02 09:34:58 +0000

    Error fixed toward the checker

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,10 +5,11 @@ at the same time with async
 import asyncio
 import heapq
 from itertools import chain
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n, max_delay):
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ Spawn wait_random n times with the specified max_delay
     and returns the list of all the delays (float values)
     """
