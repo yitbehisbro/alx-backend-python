c87f586f57cab0324385609e639fa11a4a6a0789
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-05-02 09:11:03 +0000

    Error fixed toward the checker

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,7 +3,7 @@
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """ Waits for a random delay between 0 and max_delay
     seconds and eventually returns it.
     """
