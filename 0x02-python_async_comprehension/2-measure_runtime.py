3eee34bd876a1627802aaf501393f8a7da43e8cb
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-05-03 10:07:20 +0000

    Measures the total runtime and returns it.

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Run time for four parallel comprehensions """
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ measure the total runtime and returns it """
+    start_time = time.time()
+
+    for _ in range(4):
+        await asyncio.gather(async_comprehension())
+
+    end_time = time.time()
+    total_time = end_time - start_time
+
+    return total_time / 4
