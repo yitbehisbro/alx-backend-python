3ee8ed3fa4028187b1e2a3baf01a8bdb0fbd47b1
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-05-03 09:48:49 +0000

    Collects 10 random numbers using an async comprehensing over async_generator, then return the 10 random numbers.

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ Async Comprehensions """
 import asyncio
-from typing import Generator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float, None, None]:
     """ Returns the 10 random numbers. """
     result = []
     async for i in async_generator():
