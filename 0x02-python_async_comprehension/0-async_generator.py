81c1cec1ebb73bcf8a6609d4252cf72f0e7eca27
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-05-03 09:35:42 +0000

    Yields a random number between 0 and 10

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ Async Generator """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """ Yields a random number between 0 and 10.
     """
     for _ in range(10):
