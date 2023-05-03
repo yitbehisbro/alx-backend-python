8b3fff721a02ee2cd50341788a5d0575a5f4cce2
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-05-03 09:53:44 +0000

    Error fixed toward the checker

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,8 +7,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """ Returns the 10 random numbers. """
-    result = []
-    async for i in async_generator():
-        result.append(i)
-
+    result = [num async for num in async_generator()]
     return result
