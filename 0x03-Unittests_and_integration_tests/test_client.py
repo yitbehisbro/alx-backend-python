444f5390f4c9797db0a4cbae945ff0f010cef1e2
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-05-22 13:37:16 +0300

    0x03. Unittests and Integration Tests

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Test module for client.py """
+
+import unittest
+from unittest.mock import patch, Mock, PropertyMock
+from parameterized import parameterized
+
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ Class for testing GithubOrgClient """
+
+    @parameterized.expand([
+        ("google"),
+        ("abc"),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_json):
+        """ Tests the organization info """
+        endpoint = 'https://api.github.com/orgs/{}'.format(org_name)
+        spec = GithubOrgClient(data)
+        spec.org()
+        mock_json.assert_called_once_with(endpoint)
+
+    @parameterized.expand([
+        ("random-url", {'repos_url': 'http://some_url.com'})
+    ])
+    def test_public_repos_url(self, name, result):
+        """ Test the public repo URL """
+        with patch('client.GithubOrgClient.org',
+                   PropertyMock(return_value=result)):
+            response = GithubOrgClient(name)._public_repos_url
+            self.assertEqual(response, result.get('repos_url'))
